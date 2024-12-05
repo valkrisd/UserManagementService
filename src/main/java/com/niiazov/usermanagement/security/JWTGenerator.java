b7dccd65c6cf942dfc4eeb77eac2963afb79a447
Vladislav Niiazov <vladisalv.niiazov@gmail.com>
2024-12-05 11:18:47 +0100

    1) Moving security constants to application.properties 2) Adding springdoc dependency to pom.xml

--- src/main/java/com/niiazov/usermanagement/security/JWTGenerator.java
+++ src/main/java/com/niiazov/usermanagement/security/JWTGenerator.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
@@ -16,8 +17,13 @@ import java.util.Date;
 @Component
 public class JWTGenerator {
 
+    @Value("${spring.security.jwt.secret}")
+    private String SECRET_KEY;
+    @Value("${spring.security.jwt.expiration_time}")
+    private int EXPIRATION_TIME;
+
     private SecretKey getSigningKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(SecurityConstants.SECRET_KEY);
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
@@ -28,7 +34,7 @@ public class JWTGenerator {
         JwtBuilder builder = Jwts.builder()
                 .subject(username)
                 .issuedAt(Date.from(now))
-                .expiration(Date.from(now.plusMillis(SecurityConstants.EXPIRATION_TIME)))
+                .expiration(Date.from(now.plusMillis(EXPIRATION_TIME)))
                 .signWith(getSigningKey());
 
         return builder.compact();
--- src/main/java/com/niiazov/usermanagement/security/SecurityConstants.java
+++
@@ -1,6 +0,0 @@
-package com.niiazov.usermanagement.security;
-
-public class SecurityConstants {
-    protected static final String SECRET_KEY = "SEefef878th2ol9873hcretKewefyToGenJWTsrfj6KH3G";
-    protected static final long EXPIRATION_TIME = 864_000_000;
-}
